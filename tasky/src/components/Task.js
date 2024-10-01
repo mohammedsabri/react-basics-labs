8ba4bed87e22bfcc081c2b8c0772b9a1c97f6f77
mohammed sabri <mohammedsabri228@gmail.com>
2024-10-01 04:25:05 +0100

    Mark task as don

--- tasky/src/components/Task.js
+++ tasky/src/components/Task.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
 const Task = (props) => {
     
+      <button onClick={props.markDone} className='doneButton'>Done</button> 
+
     return (
+        
         <div className="card">
             <p className="title">{props.title}</p>
             <p>Due: {props.deadline}</p>
-            <p>{props.children}</p>
+            <p>{props.description}</p>
+            <button onClick={props.markDone}>Done</button> 
+             <p style={{backgroundColor: props.done ? 'lightgrey' : '#5bb4c4'}}></p>
         </div>
+        
+
     )
 }
 
